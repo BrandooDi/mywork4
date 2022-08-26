01586138bfb225bed52e3182088acb9e3f7f56dc
BrandooDi <nikita1231w@gmail.com>
2022-08-26 16:18:00 +0300

    done

---
+++ Project/ind2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Вывести на экран большее из трёх заданных чисел.
+"""
+
+if __name__ == '__main__':
+    a, b, c = int(input()), int(input()), int(input())
+    mx = a
+
+    if b > mx:
+        mx = b
+    if c > mx:
+        mx = c
+    print(mx)
